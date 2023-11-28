7b84f49c98c4a83092a1c72614c4fd6553647025
Daniel Han <135673521+931dhan@users.noreply.github.com>
2023-11-28 15:33:54 -0500

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,22 +1,14 @@
 import dataRetriever
 
 
-def round_currency_down(price: str) -> str:
-    buy_response = ""
-    for i in range(len(price)):
-        if price[i - 1] == '.':
-            buy_response = price[0:i + 2]
-    return buy_response
-
-
 def summarize_stock_data(p_message, stock_data_dict):
     return f"""Symbol: {p_message[1].upper()}
 Date: {dataRetriever.get_stock('Date', stock_data_dict)}
-Open: ${round_currency_down(str(dataRetriever.get_stock('Open', stock_data_dict)))}
-High: ${round_currency_down(str(dataRetriever.get_stock('High', stock_data_dict)))}
-Low: ${round_currency_down(str(dataRetriever.get_stock('Low', stock_data_dict)))}
-Close: ${round_currency_down(str(dataRetriever.get_stock('Close', stock_data_dict)))}
-Adj Close: ${round_currency_down(str(dataRetriever.get_adj_close('Adj Close', stock_data_dict)))} 
+Open: ${round(dataRetriever.get_stock('Open', stock_data_dict), 2)}
+High: ${round(dataRetriever.get_stock('High', stock_data_dict), 2)}
+Low: ${round(dataRetriever.get_stock('Low', stock_data_dict), 2)}
+Close: ${round(dataRetriever.get_stock('Close', stock_data_dict), 2)}
+Adj Close: ${round(dataRetriever.get_adj_close('Adj Close', stock_data_dict), 2)} 
 Volume: {dataRetriever.get_stock('Volume', stock_data_dict)}"""
 
 
